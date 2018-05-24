d29fb39e796bac86bad4e319d9d54211c7a74ab5
Alex de Mulder <alexdemulder@gmail.com>
2018-05-24 15:22:07 +0200

    replaced position of override

--- examples/advanced/OverloadedSimulator.py
+++ examples/advanced/OverloadedSimulator.py
@@ -1,4 +1,5 @@
 from simulator import Simulator
+from simulator.util.Enums import MessageState
 
 
 class CustomSimulator(Simulator):
@@ -9,3 +10,13 @@ class CustomSimulator(Simulator):
     def __init__(self):
         super().__init__()
 
+    # overloaded
+    def handleMessage(self, message, receiver):
+        """
+        This can be overridden to implement delays, topology, failures, etc.
+        :param message:
+        :param receiver:
+        :return:
+        """
+        receiver.receiveMessage(message)
+        return MessageState.DELIVERED
--- simulator/__init__.py
+++ simulator/__init__.py
@@ -1,19 +1,9 @@
 from simulator.core.SimulationCore import SimulationCore
-from simulator.util.Enums import MessageState
-
 
 class Simulator(SimulationCore):
     """
     This is a wrapper class to expose an API
     """
-  
-    # overloaded
-    def handleMessage(self, message, receiver):
-        """
-        This can be overridden to implement delays, topology, failures, etc.
-        :param message:
-        :param receiver:
-        :return:
-        """
-        receiver.receiveMessage(message)
-        return MessageState.DELIVERED
+    
+    def __init__(self):
+        super().__init__()
